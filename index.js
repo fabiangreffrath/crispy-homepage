186d9a8ff6d88991c0b6450dfd5422a75d298a03
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-04-01 19:12:28 +0300

    add preliminary js code & update prettier rules

---
+++ index.js
@@ -0,0 +1,26 @@
+// elements
+const aboutlink = document.getElementById('aboutlink');
+const downloadlink = document.getElementById('downloadlink');
+const sourcecodelink = document.getElementById('sourcecodelink');
+const LINKS = [aboutlink, downloadlink, sourcecodelink];
+const DIVS = {};
+
+// fetch divs
+(function fetchdivs() {
+    LINKS.forEach((link) => {
+        DIVS[link.innerText] = document.getElementById(link.innerText);
+    });
+})();
+
+// add link listeners
+LINKS.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        for (const key in DIVS) {
+            if (key === e.currentTarget.innerText) {
+                DIVS[key].style.display = 'block';
+            } else {
+                DIVS[key].style.display = 'none';
+            }
+        }
+    });
+});
