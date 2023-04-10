412e3a309f412de08f56f479363020882ce0b0b7
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-04-10 07:12:05 +0300

    comment index.js file

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ import { BackgroundController } from './modules/BackgroundController.js';
 import { ElementController } from './modules/ElementController.js';
 import { PageBuilder } from './modules/PageBuilder.js';
 
+// get scattered html content & generate pages
 await PageBuilder.buildMainPages();
+
+// main content & navbar loader
 const pageController = new PageController();
+
+// modal images
 const modalController = new ModalController();
+
+// random pixelated background
 const backgroundController = new BackgroundController();
+
+// page elements, such as toggle-buttons
 const elementController = new ElementController();
