d991b625b39f1b45909047fdb586bf79862df022
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-04-24 16:18:12 +0300

    update markdown parser

--- modules/PageBuilder.js
+++ modules/PageBuilder.js
@@ -1,4 +1,4 @@
-const md = window.markdownit();
+const md = window.marked;
 
 const CHEATS_URL = 'https://raw.githubusercontent.com/fabiangreffrath/crispy-doom/master/docs/cheats.md';
 const PARAMS_URL = 'https://raw.githubusercontent.com/fabiangreffrath/crispy-doom/master/docs/params.md';
@@ -33,21 +33,21 @@ export class PageBuilder {
 
     static async fetchCheats() {
         const res = await fetch(CHEATS_URL);
-        document.getElementById('cheats').innerHTML = md.render(await res.text());
+        document.getElementById('cheats').innerHTML = md.parse(await res.text());
     }
 
     static async fetchParams() {
         const res = await fetch(PARAMS_URL);
-        document.getElementById('params').innerHTML = md.render(await res.text());
+        document.getElementById('params').innerHTML = md.parse(await res.text());
     }
 
     static async fetchCrispness() {
         const res = await fetch(CRISPNESS_URL);
-        document.getElementById('crispness-menu').innerHTML = md.render(await res.text());
+        document.getElementById('crispness-menu').innerHTML = md.parse(await res.text());
     }
 
     static async fetchCompatibility() {
         const res = await fetch(COMPATIBILITY_URL);
-        document.getElementById('compatibility').innerHTML = md.render(await res.text());
+        document.getElementById('compatibility').innerHTML = md.parse(await res.text());
     }
 }
