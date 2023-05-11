c76dd2cf6de43a464e3edd2434d7cc98c300a01b
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-05-11 10:43:16 +0300

    update dev script

--- dev.sh
+++ dev.sh
@@ -8,7 +8,7 @@ tmux new-session -d -s $SESSION
 tmux rename-window -t $SESSION:0 'docker'
 tmux new-window -t $SESSION:1 -n 'code'
 
-sleep 1 # wait until shell has loaded on all panes
+sleep 3 # wait until shell has loaded on all panes
 
 # send appropriate keys to each window
 tmux send-keys -t $SESSION:0 'docker-compose -f nginx.yaml up' C-m
